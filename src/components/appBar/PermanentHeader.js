a6732aa007d9a90d2085fceb7fb10788641560a8
karinka <kharinakaryna@gmail.com>
2020-10-30 10:34:48 +0200

    created PermanentHeader component

---
+++ src/components/appBar/PermanentHeader.js
@@ -0,0 +1,33 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import {useStyles} from "./MenuInLeft";
+import IconButton from "@material-ui/core/IconButton";
+import {Home} from "@material-ui/icons";
+import {Link} from "react-router-dom";
+import {MAIN_PAGE} from "../../links";
+
+export default () => {
+    const classes = useStyles();
+
+    return (
+        <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+                <Link to={MAIN_PAGE}>
+                    <IconButton>
+                        <Home className={classes.homeBtn}/>
+                    </IconButton>
+                </Link>
+
+                <Typography
+                    noWrap
+                    variant="h6"
+                    className={classes.header}
+                >
+                    BOOS KIBOOS
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
