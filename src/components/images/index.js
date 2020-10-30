c05afa0e5041e9bbc8033963bf05772ed5213c07
karinka <kharinakaryna@gmail.com>
2020-10-30 15:42:51 +0200

    renamed props

--- src/components/images/index.js
+++ src/components/images/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import {Container} from "@material-ui/core";
-import CreateResourceForm from "../../containers/CreateResourceForm";
+import CreateResourceForm from "../../containers/images/CreateResourceForm";
 import ImagesTable from "./ImagesTable";
 
-export default function ({resources, onDelete}) {
+export default function ({images, onDelete}) {
     return (
         <div>
             <Toolbar/>
             <Container>
                 <CreateResourceForm/>
                 <ImagesTable
-                    resources={resources}
+                    images={images}
                     onDelete={onDelete}
                 />
             </Container>
