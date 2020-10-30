2d95e72025a77e89289c6a1b174cc60f01a64d1d
karinka <kharinakaryna@gmail.com>
2020-10-30 10:35:46 +0200

    created CreateResourceForm component

---
+++ src/components/images/CreateResourceForm.js
@@ -0,0 +1,91 @@
+import React from "react";
+import {Button} from "@material-ui/core";
+import {Add} from "@material-ui/icons";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import TextField from "@material-ui/core/TextField";
+import {useStyles} from "./ImagesTable";
+import Grid from "@material-ui/core/Grid";
+
+export default ({
+                    onSave,
+                    name, isValidName, onChangeName,
+                    url, isValidUrl, onChangeUrl,
+                    isOpen, onChangeIsOpen
+                }) => {
+
+    const classes = useStyles();
+
+    const handleOpen = () => onChangeIsOpen(true);
+    const handleClose = () => onChangeIsOpen(false);
+    const isDisabledButtonSave = () => !isValidUrl && !isValidName;
+
+    return (
+        <div>
+            <Button variant="contained"
+                    color="primary"
+                    size="large"
+                    className={classes.button}
+                    startIcon={<Add/>}
+                    onClick={handleOpen}
+            >
+                Add resource
+            </Button>
+            <Modal
+                className={classes.modal}
+                open={isOpen}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={isOpen}>
+                    <Grid container spacing={3} className={classes.paper}>
+                        <Grid item xs={12}>
+                            <TextField
+                                label="Enter the name of resource"
+                                variant="outlined"
+                                value={name}
+                                required
+                                fullWidth
+                                error={!isValidName}
+                                onChange={e => onChangeName(e.target.value)}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField
+                                label="Enter the URL of resource"
+                                variant="outlined"
+                                value={url}
+                                required
+                                fullWidth
+                                error={!isValidUrl}
+                                onChange={e => onChangeUrl(e.target.value)}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Button
+                                className={classes.button}
+                                fullWidth
+                                onClick={() => {
+                                    onSave({name, url});
+                                    handleClose();
+                                }}
+                                disabled={isDisabledButtonSave()}
+                                color="primary"
+                                variant="contained"
+                            >
+                                Save resource
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Fade>
+            </Modal>
+        </div>
+    )
+}
