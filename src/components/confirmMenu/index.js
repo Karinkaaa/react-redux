a90cdef39e83aec19eebafc69a25d710c0ceffdb
karinka <kharinakaryna@gmail.com>
2020-10-30 11:59:18 +0200

    replaced menu items "Yes/No" on icons

--- src/components/confirmMenu/index.js
+++ src/components/confirmMenu/index.js
@@ -1,22 +1,40 @@
 import React from "react";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
+import {Check, Close} from "@material-ui/icons";
+import {makeStyles} from "@material-ui/core/styles";
 
-export default ({anchorEl, handleClose, onAccept}) => (
-    <Menu
-        open={Boolean(anchorEl)}
-        anchorEl={anchorEl}
-        id="menu-list-grow"
-        onClose={handleClose}
-        variant="selectedMenu"
-    >
-        <MenuItem onClick={() => {
-            onAccept(anchorEl.dataset.id);
-            handleClose();
-        }}
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    }
+}));
+
+export default ({anchorEl, handleClose, onAccept}) => {
+    const classes = useStyles();
+
+    return (
+        <Menu
+            open={Boolean(anchorEl)}
+            anchorEl={anchorEl}
+            id="menu-list-grow"
+            onClose={handleClose}
+            variant="selectedMenu"
         >
-            Yes
-        </MenuItem>
-        <MenuItem onClick={handleClose}>No</MenuItem>
-    </Menu>
-)
+            <div className={classes.root}>
+                <MenuItem
+                    onClick={() => {
+                        onAccept(anchorEl.dataset.id);
+                        handleClose();
+                    }}
+                >
+                    <Check color="primary"/>
+                </MenuItem>
+
+                <MenuItem onClick={handleClose}>
+                    <Close color="secondary"/>
+                </MenuItem>
+            </div>
+        </Menu>
+    )
+}
