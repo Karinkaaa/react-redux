71a9c84e5422271db9f49bf33b26f4129c839e71
karinka <kharinakaryna@gmail.com>
2020-10-30 10:40:22 +0200

    added new method for name validation

--- src/validation.js
+++ src/validation.js
@@ -1,5 +1,9 @@
-export function isValidImageUrl(url) {
+export function isValidName(name) {
+    const pattern = /^[A-Za-z]+[\s]?[A-Za-z]+$/;
+    return pattern.test(name) && name.length > 1;
+}
 
+export function isValidImageUrl(url) {
     const patternBase64 = /^data:image\/jpeg;base64,(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$/;
     const patternURL = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=]+$/;
     return patternBase64.test(url) || patternURL.test(url);
