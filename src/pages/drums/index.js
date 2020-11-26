babf9097bd3527ca4f2beada90846e9bdad74f38
karinka <kharinakaryna@gmail.com>
2020-11-26 11:41:50 +0200

    refactoring

---
+++ src/pages/drums/index.js
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
+// fake data generator
+const getItems = count =>
+    Array.from({ length: count }, (v, k) => k).map(k => ({
+        id: `item-${k}`,
+        content: `item ${k}`
+    }));
+
+// a little function to help us with reordering the result
+const reorderItems = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: "none",
+    padding: grid * 2,
+    margin: 1,
+
+    // change background colour if dragging
+    background: isDragging ? "darkcyan" : "#335068",
+
+    // styles we need to apply on draggables
+    ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? "lightblue" : "lightgrey",
+    padding: 2,
+    width: 500
+});
+
+class Drums extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: getItems(10)
+        };
+        this.onDragAndDrop = this.onDragAndDrop.bind(this);
+    }
+
+    onDragAndDrop(result) {
+        // dropped outside the list
+        if (!result.destination) {
+            return;
+        }
+
+        const items = reorderItems(
+            this.state.items,
+            result.source.index,
+            result.destination.index
+        );
+
+        this.setState({
+            items
+        });
+    }
+
+    // Normally you would want to split things out into separate components.
+    // But in this example everything is just done in one place for simplicity
+    render() {
+        return (
+            <DragDropContext onDragEnd={this.onDragAndDrop}>
+                <Droppable droppableId="droppable">
+                    {(provided, snapshot) => (
+                        <div
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                            style={getListStyle(snapshot.isDraggingOver)}
+                        >
+                            {this.state.items.map((item, index) => (
+                                <Draggable key={item.id} draggableId={item.id} index={index}>
+                                    {(provided, snapshot) => (
+                                        <div
+                                            ref={provided.innerRef}
+                                            {...provided.draggableProps}
+                                            {...provided.dragHandleProps}
+                                            style={getItemStyle(
+                                                snapshot.isDragging,
+                                                provided.draggableProps.style
+                                            )}
+                                        >
+                                            {item.content}
+                                            <button onClick={() => console.log("onClickBtn", item.content)}>
+                                                click me
+                                            </button>
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
+        );
+    }
+}
+
+export default Drums
