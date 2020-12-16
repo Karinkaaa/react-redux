d98aa2ec94f29a056a917e6d5ff796a229a8e848
karinka <95480kiss>
2020-12-16 11:40:55 +0200

    added new method for file reading

--- src/utils/methods.js
+++ src/utils/methods.js
@@ -80,3 +80,17 @@ export function filteringSortingPagingOfArray(arr, {
 
     return { data: result, count: arr.length };
 }
+
+export function readFile(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            resolve(event?.target?.result);
+        };
+        reader.onerror = (event) => {
+            reader.abort();
+            reject(event);
+        };
+        reader.readAsDataURL(file);
+    });
+}
