7317b4084a7070a5e0c1e5d2ea83dae959aa6ce4
karinka <95480kiss>
2021-01-13 10:50:24 +0200

    created new component for display rule combination in the table

---
+++ src/components/ruleTable/RulesGrid.js
@@ -0,0 +1,58 @@
+import React from "react";
+import uuid from "react-uuid";
+import PropTypes from "prop-types";
+import { Container, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { RuleItems } from "../drumsForRules/RuleItems";
+import { RadioButtonChecked, RadioButtonUnchecked } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        maxWidth: 150,
+        marginLeft: 0
+    },
+    gridContainer: {
+        margin: 0
+    }
+}));
+
+const RulesGrid = ({ ruleItems, conditions }) => {
+    const classes = useStyles();
+
+    return (
+        <Container maxWidth={"xs"} className={classes.container}>
+            <Grid container className={classes.gridContainer}>
+                {
+                    ruleItems.map(rule => {
+                        const ruleConditions = rule.map(conditions => conditions);
+
+                        return ruleConditions.map(ruleCondition => {
+                                const { x, y } = ruleCondition;
+                                const element = conditions.find(item => item.x === x && item.y === y);
+
+                                return (
+                                    <Grid item key={uuid()}>
+                                        {element ?
+                                            <RadioButtonChecked fontSize={"small"}/>
+                                            :
+                                            <RadioButtonUnchecked fontSize={"small"}/>}
+                                    </Grid>
+                                );
+                            }
+                        );
+                    })
+                }
+            </Grid>
+        </Container>
+    );
+};
+
+RulesGrid.defaultProps = {
+    ruleItems: RuleItems
+};
+
+RulesGrid.propTypes = {
+    conditions: PropTypes.array.isRequired
+};
+
+export default RulesGrid;
