f6a8229f7199178926d1808f9edc1d5ebace1ae8
karinka <kharinakaryna@gmail.com>
2021-04-07 21:22:01 +0300

    updated LayerPage component

--- src/pages/layers/index.js
+++ src/pages/layers/index.js
@@ -3,8 +3,17 @@ import React from "react";
 import { Button, Container, Grid } from "@material-ui/core";
 import Toolbar from "@material-ui/core/Toolbar";
 import { Add } from "@material-ui/icons";
+import TablePagination from "@material-ui/core/TablePagination";
+import LayersTable from "../../components/layersTable";
 
-const Layers = () => {
+const Layers = ({
+                    layers, count, onDelete, onClickPutLayerToForm, pagination, onChangePage, onChangeLimit,
+                    sorting, onChangeSort, onChangeDirection, onChangeFilterValue, onDeleteNestedElement, onDragAndDrop
+                }) => {
+    const { page, limit } = pagination;
+
+    const handleChangeLayerPage = (event, newPage) => onChangePage(newPage);
+    const handleChangeLayerLimit = (event) => onChangeLimit(parseInt(event.target.value, 10));
 
     return (
         <div>
@@ -22,26 +31,36 @@ const Layers = () => {
                         </Button>
                     </Grid>
 
-                    {/*<Grid item xs={5}>*/}
-                    {/*    <TablePagination*/}
-                    {/*        style={{ color: "#cfeaff" }}*/}
-                    {/*        component="div"*/}
-                    {/*        color="primary"*/}
-                    {/*        colSpan={6}*/}
-                    {/*        page={page}*/}
-                    {/*        count={count}*/}
-                    {/*        rowsPerPage={limit}*/}
-                    {/*        rowsPerPageOptions={[4, 8, 12, 16, 20, 40, 60, 80, 100]}*/}
-                    {/*        onChangePage={handleChangeImagePage}*/}
-                    {/*        onChangeRowsPerPage={handleChangeImageLimit}*/}
-                    {/*    />*/}
-                    {/*</Grid>*/}
+                    <Grid item xs={5}>
+                        <TablePagination
+                            style={{ color: "#cfeaff" }}
+                            component="div"
+                            color="primary"
+                            colSpan={6}
+                            page={page}
+                            count={count}
+                            rowsPerPage={limit}
+                            rowsPerPageOptions={[4, 8, 12, 16, 20, 40, 60, 80, 100]}
+                            onChangePage={handleChangeLayerPage}
+                            onChangeRowsPerPage={handleChangeLayerLimit}
+                        />
+                    </Grid>
 
-                    <Grid item xs={1}/>
+                    <LayersTable
+                        layers={layers}
+                        onDelete={onDelete}
+                        onClickPutLayerToForm={onClickPutLayerToForm}
+                        sorting={sorting}
+                        onChangeSort={onChangeSort}
+                        onChangeDirection={onChangeDirection}
+                        onChangeFilterValue={onChangeFilterValue}
+                        onDeleteNestedElement={onDeleteNestedElement}
+                        onDragAndDrop={onDragAndDrop}
+                    />
                 </Grid>
             </Container>
         </div>
-    )
+    );
 };
 
 // Layers.propTypes = {};
