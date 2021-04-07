bb2405f3911d1a5ad66fcd3302b7ed9cb4078cc8
karinka <kharinakaryna@gmail.com>
2021-04-07 21:21:37 +0300

    added new fields and functions

--- src/containers/layerPage/index.js
+++ src/containers/layerPage/index.js
@@ -1,10 +1,34 @@
 import { connect } from "react-redux";
-import { addLayer, deleteLayer, updateLayer } from "../../actions/layerComponent";
 import Layers from "../../pages/layers";
+import { filteringSortingPagingOfArray } from "../../utils/methods";
+import { putLayerToForm } from "../../actions/layerForm";
+import {
+    addLayer,
+    changeLayerFilterValue,
+    changeLayerLimit,
+    changeLayerPage,
+    changeLayerSort,
+    deleteLayer,
+    deleteNestedLayerElement,
+    dragAndDrop,
+    updateLayer
+} from "../../actions/layerComponent";
 
 const mapStateToProps = (state) => {
+    const { data: layers, count } = filteringSortingPagingOfArray(state.layers.layerList,
+        {
+            pagination: state.layers.pagination,
+            sorting: state.layers.sorting,
+            filters: state.layers.filters
+        }
+    );
+
     return {
-        layerList: state.layers.layerList
+        layers,
+        count,
+        pagination: state.layers.pagination,
+        sorting: state.layers.sorting,
+        filters: state.layers.filters
     };
 };
 
@@ -12,7 +36,14 @@ const mapDispatchToProps = (dispatch) => {
     return {
         onAdd: (layer) => dispatch(addLayer(layer)),
         onDelete: (id) => dispatch(deleteLayer(id)),
-        onChange: (layer) => dispatch(updateLayer(layer))
+        onChange: (layer) => dispatch(updateLayer(layer)),
+        onChangePage: (page) => dispatch(changeLayerPage(page)),
+        onChangeLimit: (limit) => dispatch(changeLayerLimit(limit)),
+        onChangeSort: (field) => dispatch(changeLayerSort(field)),
+        onChangeFilterValue: (props) => dispatch(changeLayerFilterValue(props)),
+        onClickPutLayerToForm: (props) => dispatch(putLayerToForm(props)),
+        onDeleteNestedElement: (id, nestedId) => dispatch(deleteNestedLayerElement(id, nestedId)),
+        onDragAndDrop: (result, id) => dispatch(dragAndDrop(result, id))
     };
 };
 
