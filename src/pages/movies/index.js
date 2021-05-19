420a4357196917beb131c0faa7a822a7e340aca8
karinka <kharinakaryna@gmail.com>
2021-05-19 17:04:47 +0300

    updated Movies page

--- src/pages/movies/index.js
+++ src/pages/movies/index.js
@@ -1,101 +1,101 @@
 import React, { useEffect } from "react";
-import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import { Button, Container, Table, TableBody, TableCell, TableRow } from "@material-ui/core";
-import TablePagination from "@material-ui/core/TablePagination";
-import IconButton from "@material-ui/core/IconButton";
-import { Delete, Update } from "@material-ui/icons";
-import { MOVIE_CREATE_FORM, MOVIE_UPDATE_FORM } from "../../utils/links";
-import {
-    changeMovieLimit,
-    changeMoviePage,
-    clearMovieForm,
-    getMoviesSaga,
-    removeMovieSaga
-} from "../../actions/movies";
+import { Button, Container, Grid, TablePagination, Toolbar } from "@material-ui/core";
+import MovieTable from "../../components/movieTable";
+import { MOVIE_CREATE_FORM } from "../../utils/links";
 
-const Movie = ({ movies, count, pagination, getMovies, removeMovie, clearForm, onChangePage, onChangeLimit }) => {
+const Movie = ({
+                   movies, count, pagination, sorting, filters, getMovies, clearForm, removeMovie, onChangeIsOpen,
+                   onChangePage, onChangeLimit, onChangeSort, onChangeFilterValue, onClickPutResourceToForm
+               }) => {
     const { page, limit } = pagination;
 
     const onRemove = (id) => removeMovie(id);
-    const handleChangeMoviePage = (event, newPage) => onChangePage(newPage);
-    const handleChangeMovieLimit = (event) => onChangeLimit(parseInt(event.target.value, 10));
+    const handleChangeImagePage = (event, newPage) => onChangePage(newPage);
+    const handleChangeImageLimit = (event) => onChangeLimit(parseInt(event.target.value, 10));
 
     useEffect(() => {
         getMovies();
-    }, [pagination]);
+    }, [pagination, sorting, filters]);
 
     return (
-        <Container style={{ marginTop: 50 }}>
-            <Link to={MOVIE_CREATE_FORM}>
-                <Button
-                    variant={"contained"}
-                    color={"primary"}
-                    onClick={clearForm}
-                >
-                    Create movie
-                </Button>
-            </Link>
+        <div>
+            <Toolbar/>
+            <Container>
+                <Grid container>
+                    <Grid item xs={5}>
+                        <Link to={MOVIE_CREATE_FORM}>
+                            <Button
+                                variant={"contained"}
+                                color={"primary"}
+                                onClick={clearForm}
+                            >
+                                Create movie
+                            </Button>
+                        </Link>
+                    </Grid>
 
-            <TablePagination
-                style={{ color: "#cfeaff" }}
-                component="div"
-                color="primary"
-                colSpan={6}
-                page={page}
-                count={count}
-                rowsPerPage={limit}
-                rowsPerPageOptions={[4, 8, 12, 16, 20, 40, 60, 80, 100]}
-                onChangePage={handleChangeMoviePage}
-                onChangeRowsPerPage={handleChangeMovieLimit}
-            />
+                    <Grid item xs={5}>
+                        <TablePagination
+                            style={{ color: "#cfeaff" }}
+                            component={"div"}
+                            color={"primary"}
+                            colSpan={6}
+                            page={page}
+                            count={count}
+                            rowsPerPage={limit}
+                            rowsPerPageOptions={[4, 8, 12, 16, 20, 40, 60, 80, 100]}
+                            onChangePage={handleChangeImagePage}
+                            onChangeRowsPerPage={handleChangeImageLimit}
+                        />
+                    </Grid>
 
-            <Table>
-                <TableBody>
-                    {
-                        movies.map(({ id, name, year, rating }) =>
-                            <TableRow key={id}>
-                                <TableCell>{id}</TableCell>
-                                <TableCell>{name}</TableCell>
-                                <TableCell>{year}</TableCell>
-                                <TableCell>{rating}</TableCell>
-
-                                <TableCell>
-                                    <Link to={MOVIE_UPDATE_FORM.replace(":id", id)}>
-                                        <IconButton><Update/></IconButton>
-                                    </Link>
-                                </TableCell>
-
-                                <TableCell>
-                                    <IconButton onClick={() => onRemove(id)}><Delete/></IconButton>
-                                </TableCell>
-                            </TableRow>
-                        )
-                    }
-                </TableBody>
-            </Table>
-        </Container>
+                    <MovieTable
+                        movies={movies}
+                        sorting={sorting}
+                        onDelete={onRemove}
+                        onChangeSort={onChangeSort}
+                        onChangeFilterValue={onChangeFilterValue}
+                        onChangeIsOpen={onChangeIsOpen}
+                        onClickPutResourceToForm={onClickPutResourceToForm}
+                    />
+                </Grid>
+            </Container>
+        </div>
     );
 };
 
-const mapStateToProps = (state) => {
-    return {
-        movies: state.movies.movieList,
-        count: state.movies.count,
-        pagination: state.movies.pagination,
-        sorting: state.movies.sorting,
-        filters: state.movies.filters
-    };
+Movie.propTypes = {
+    movies: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string,
+            name: PropTypes.string.isRequired,
+            year: PropTypes.number.isRequired,
+            rating: PropTypes.string.isRequired
+        }).isRequired
+    ).isRequired,
+    count: PropTypes.number.isRequired,
+    pagination: PropTypes.shape({
+            page: PropTypes.number.isRequired,
+            limit: PropTypes.number.isRequired
+        }
+    ).isRequired,
+    sorting: PropTypes.shape({
+            field: PropTypes.string.isRequired,
+            direction: PropTypes.oneOf(["asc", "desc"]).isRequired
+        }
+    ).isRequired,
+    filters: PropTypes.object.isRequired,
+    getMovies: PropTypes.func.isRequired,
+    clearForm: PropTypes.func.isRequired,
+    removeMovie: PropTypes.func.isRequired,
+    onChangePage: PropTypes.func.isRequired,
+    onChangeLimit: PropTypes.func.isRequired,
+    onChangeSort: PropTypes.func.isRequired,
+    onChangeFilterValue: PropTypes.func.isRequired,
+    onChangeIsOpen: PropTypes.func,
+    onClickPutResourceToForm: PropTypes.func
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        getMovies: () => dispatch(getMoviesSaga()),
-        removeMovie: (id) => dispatch(removeMovieSaga(id)),
-        clearForm: () => dispatch(clearMovieForm()),
-        onChangePage: (page) => dispatch(changeMoviePage(page)),
-        onChangeLimit: (limit) => dispatch(changeMovieLimit(limit))
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Movie);
+export default Movie;
