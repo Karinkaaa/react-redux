db72e758f8948f2c72d593de70f5ccc247fc2530
karinka <95480kiss>
2021-06-16 12:45:18 +0300

    created constants for GroupedResources and updated the component

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,11 @@
+// view
 export const GRID = "GRID";
 export const TABLE = "TABLE";
 
+// speed
 export const INITIAL_SPEED = 0.01;
+
+// for grouped resources
+export const IMAGES_GROUP = "IMAGES";
+export const ANIMATIONS_GROUP = "ANIMATIONS";
+export const DRAGON_BONES_GROUP = "DRAGON_BONES";
